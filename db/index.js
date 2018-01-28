1a402fa5bec1b7dfe223e3b887565c7a109a7492
Sue Roh <sr3022@columbia.edu>
2018-01-28 16:31:20 -0500

    Added client conenction

--- db/index.js
+++ db/index.js
@@ -2,12 +2,20 @@ const debug = require('debug')('sql')
 const chalk = require('chalk')
 const Sequelize = require('sequelize')
 const Promise = require('bluebird')
+const { Client } = require('pg')
 
 const dbName = 'pilates'
 const url = process.env.DATABASE_URL
   // | `postgres://localhost:5432/${dbName}`
 
 console.log(chalk.yellow(`Opening database connection to ${url}`));
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+})
+
+client.connect()
 
 const db = new Sequelize(url, {
   logging: debug,
