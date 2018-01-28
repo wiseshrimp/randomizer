85d127662ea0318ddae554ba94e59ee4014dd6c1
Sue Roh <sr3022@columbia.edu>
2018-01-28 17:11:57 -0500

    Sequelize config

--- db/models/Member.js
+++ db/models/Member.js
@@ -1,18 +1,18 @@
 const Sequelize = require('sequelize')
 
-const Member = global.db.define('members', {
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  medium: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  updated_at: {
-    type: Sequelize.DATE
-  }
-})
+// const Member = global.db.define('members', {
+//   name: {
+//     type: Sequelize.STRING,
+//     allowNull: false
+//   },
+//   medium: {
+//     type: Sequelize.STRING,
+//     allowNull: false
+//   },
+//   updated_at: {
+//     type: Sequelize.DATE
+//   }
+// })
 
 module.exports = function (sequelize, DataTypes) {
   return sequelize.define('members', {
