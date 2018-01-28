ed2d4450ad22fc6819122d99da293e2b73220f9b
Sue Roh <sr3022@columbia.edu>
2018-01-28 16:40:47 -0500

    Db config

--- db/index.js
+++ db/index.js
@@ -15,8 +15,6 @@ if (process.env.DATABASE_URL) {
   db = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     protocol: 'postgress',
-    port: match[4],
-    host: match[3],
     logging: true
   })
 } else {
