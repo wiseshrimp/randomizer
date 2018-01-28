286a6061df66410fd8090a92f1221b43e85276b1
Sue Roh <sr3022@columbia.edu>
2018-01-28 17:04:03 -0500

    Db sequelize config

--- db/index.js
+++ db/index.js
@@ -7,30 +7,55 @@ const { Client } = require('pg')
 const dbName = 'pilates'
 // const url = `postgres://localhost:5432/${dbName}`
 
-const url = process.env.DATABASE_URL || `postgres://localhost:5432/${name}`
+var sequelize
 
-const db = new Sequelize(url, {
-  logging: debug,
-  define: {
-    underscored: true,
-    freezeTableName: true,
-    timestamps: true
-  }
-})
+if (process.env.HEROKU_POSTGRESQL_COBALT_URL) {
+  sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_COBALT_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    logging: true
+  })
+} else {
+  sequelize = new Sequelize(`postgres://localhost:5432/${name}`, {
+    logging: debug,
+    define: {
+      underscored: true,
+      freezeTableName: true,
+      timestamps: true
+    }
+  })
+}
+
+const db = global.db = {
+  Sequelize: Sequelize,
+  sequelize: sequelize,
+  User: sequelize.import(__dirname + '/models/Member')
+}
+
+// const url = process.env.DATABASE_URL || `postgres://localhost:5432/${name}`
+
+// const db = new Sequelize(`postgres://localhost:5432/${name}`, {
+//   logging: debug,
+//   define: {
+//     underscored: true,
+//     freezeTableName: true,
+//     timestamps: true
+//   }
+// })
 
 
-const Member = db.define('members', {
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  medium: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  updated_at: {
-    type: Sequelize.DATE
-  }
-})
+// const Member = db.define('members', {
+//   name: {
+//     type: Sequelize.STRING,
+//     allowNull: false
+//   },
+//   medium: {
+//     type: Sequelize.STRING,
+//     allowNull: false
+//   },
+//   updated_at: {
+//     type: Sequelize.DATE
+//   }
+// })
 
 module.exports = db
